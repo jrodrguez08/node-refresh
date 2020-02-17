8467c82799b378f779fbb9b8d7def6733de279b4
jrodrguez08 <jrodrguez08@gmail.com>
2020-02-17 15:52:01 -0600

    Adds create store method

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { catchErrors } = require('../handlers/errorHandlers');
 
 const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homePage);
 
-
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
